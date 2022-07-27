6db3f4f207b4b5e1c6ca320dcd1aacafd3aa5a94
JMS <thatdudejoss@gmail.com>
2022-07-27 16:55:07 +0200

    Initialized quick.nvim basic nvim setup

---
+++ .config/nvim/init.lua
@@ -0,0 +1,40 @@
+require('basics')
+require('colors')
+require('telescope-config')
+require('coc-config')
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "all",
+  ignore_install = { "phpdoc" },
+  context_commentstring = {
+    enable = true
+  },
+  highlight = {
+    enable = true,
+    disable = { "lua" }
+  },
+  indent = {
+    enable = true
+  }
+}
+
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use {'neoclide/coc.nvim', branch = 'release'}
+  use 'folke/tokyonight.nvim'
+  use 'nvim-treesitter/nvim-treesitter' 
+  use 'tpope/vim-commentary'
+  use 'JoosepAlviste/nvim-ts-context-commentstring'
+  use 'lukas-reineke/indent-blankline.nvim'
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use { "nvim-telescope/telescope-file-browser.nvim" }
+  use {
+    "ur4ltz/surround.nvim",
+    config = function()
+      require"surround".setup {mappings_style = "surround"}
+    end
+  }
+end)
